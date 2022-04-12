eb8e538f2f4a3e897029c37581d5e459fd51879a
michaelhos412 <u3560321@connect.hku.hk>
2022-04-12 21:19:50 +0800

    scoring and collision update

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CharacterManager : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -10,6 +11,9 @@ public class CharacterManager : MonoBehaviour
 	bool canmove = true;
 	int line = 1;
 	int targetLine = 1;
+	public static int life = 3;
+	public static int score = 0;
+
 
 	void Start () {
 		charController = gameObject.GetComponent<CharacterController>();
@@ -61,4 +65,54 @@ public class CharacterManager : MonoBehaviour
 			movec.x = 4f;
 		}
 	}
+
+	void OnCollisionEnter(Collision obj) {
+
+		string name = obj.gameObject.name;
+		string this_name = gameObject.name;
+		Debug.Log("Collided with " + name);
+		Debug.Log("The current object is " + this_name);
+		// if player collided with vitamin
+		if (name == "vitamin"){
+			Destroy(obj.gameObject);
+			score += 100;
+		}
+
+		if (name == "mask"){
+			Destroy(obj.gameObject);
+			score += 200;
+		}
+		
+		if (name == "vaccine"){
+			Destroy(obj.gameObject);
+			score += 300;
+		}
+
+		if (name == "alpha_variant"){
+			Destroy(obj.gameObject);
+			life -= 1;
+			score -= 100;
+		}
+
+		if (name == "delta_variant"){
+			Destroy(obj.gameObject);
+			life -= 1;
+			score -= 200;
+		}
+		
+		if (name == "omicron_variant"){
+			Destroy(obj.gameObject);
+			score -= 300;
+		}
+
+	}
+	void OnGUI () {
+	GUI.Label(new Rect(100, 0, 400, 400), "Score: " +score);
+	GUI.Label(new Rect(100, 50, 400, 400), "Life: " + life);
+	if (life <= 0 ){
+		GUI.Label(new Rect(Screen.width/2-100,Screen.height/2,500,100),"Game Over");
+		Time.timeScale = 0;
+	}
+
+}
 }
