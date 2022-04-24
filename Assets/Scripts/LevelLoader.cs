36de87359beab1e76e95dacf41600f6f993eef22
michaelhos412 <u3560321@connect.hku.hk>
2022-04-25 03:06:22 +0800

    main menu navigation

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject level_select = null;
+
     public void LoadScene(string level)
     { 
         SceneManager.LoadScene("Level1");
@@ -17,14 +19,26 @@ public class LevelLoader : MonoBehaviour
 
     public void LoadLevel1(){
         // Load Level One
-        SceneManager.LoadScene("Level 1");
+        SceneManager.LoadScene("Level1");
     }
 
     public void LoadLevel2(){
-        SceneManager.LoadScene("Level 2");
+        SceneManager.LoadScene("Level2");
     }
 
     public void LoadLevel3(){
-        SceneManager.LoadScene("Level 3");
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void LoadLevelSelect(){
+        level_select.SetActive(true);
+    }
+
+    public void BackButton1(){
+        level_select.SetActive(false);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
     }
 }
